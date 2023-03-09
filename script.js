543f5dd2666b5d6b26733d87191c06d18fcb2ff2
RodriigoCampos <116584102+RodriigoCampos@users.noreply.github.com>
2023-03-09 18:44:39 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+const screen1 = document.querySelector(".screen1")
+const screen2 = document.querySelector(".screen2")
+const btnTry = document.querySelector("#btnTry")
+const btnReset = document.querySelector("#btnReset")
+let randomNumber = Math.round(Math.random() * 10)
+let xAttempts = 1
+
+btnTry.addEventListener('click', handleTryClick)
+btnReset.addEventListener('click', handleResetClick)
+document.addEventListener('keydown', function(e) {
+   if (e.key == 'Enter' && screen1.classList.contains('hide')) {
+    handleResetClick()
+   }
+})
+
+function handleTryClick(event) {
+    event.preventDefault() //não faça o padrão
+
+    const inputNumber = document.querySelector("#inputNumber")
+
+    if (Number(inputNumber.value) == randomNumber) {
+       toggleScreen()
+
+        document.querySelector(".screen2 h2").innerText = `Você acertou em ${xAttempts} tentativas!!`
+       }
+    
+    inputNumber.value = ""
+    xAttempts++
+}
+
+function handleResetClick (event) {
+    toggleScreen()
+
+    xAttempts = 1
+    randomNumber = Math.round(Math.random() * 10)
+}
+
+function toggleScreen () {
+    screen1.classList.toggle("hide")
+    screen2.classList.toggle("hide")
+
+}
+
+
